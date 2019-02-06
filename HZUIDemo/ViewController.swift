03330efd6eda521824a7949d21cfa2ec07846022
heze <895315401@qq.com>
2019-02-06 20:56:31 +0800

    Initial Commit

---
+++ HZUIDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HZUIDemo
+//
+//  Created by 何泽的mac on 2019/2/6.
+//  Copyright © 2019年 何泽的mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ HZUIDemoTests/HZUIDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  HZUIDemoTests.swift
+//  HZUIDemoTests
+//
+//  Created by 何泽的mac on 2019/2/6.
+//  Copyright © 2019年 何泽的mac. All rights reserved.
+//
+
+import XCTest
+@testable import HZUIDemo
+
+class HZUIDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
